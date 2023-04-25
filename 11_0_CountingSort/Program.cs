23dd09a237e4866e9110edebfb65e7afe6699b72
Deskardo <kalugin.aleksei.v@gmail.com>
2023-04-25 14:28:22 +0300

    task 11_0 CountingSort fin

---
+++ 11_0_CountingSort/Program.cs
@@ -0,0 +1,36 @@
+﻿void InputArray(int[] array)
+{
+  for (int i = 0; i < array.Length; i++)
+    array[i] = new Random().Next(1, 101);
+}
+
+
+void CompetetionArray(int[] array, int[] helpArray)
+{
+  for (int i = 0; i < array.Length; i++)
+    helpArray[array[i]] += 1;
+}
+
+
+void SortArray(int[] array, int[] helpArray)
+{
+  int j = 0;
+  for (int i = 1; i <= 100; i++)
+    while (helpArray[i] > 0)
+    {
+      array[j] = i;
+      j++;
+      helpArray[i]--;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[] array = new int[n];
+int[] helpArray = new int[101];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+CompetetionArray(array, helpArray);
+SortArray(array, helpArray);
+Console.WriteLine($"Отсортированный массив: [{string.Join(", ", array)}]");
