e0c5828c9397df0501c3b371d8c3532049dc25b5
Deskardo <kalugin.aleksei.v@gmail.com>
2023-04-25 15:19:09 +0300

    task 11_1_input fin

--- 11_1_последовательный ввод данных/Program.cs
+++ 11_1_последовательный ввод данных/Program.cs
@@ -1,5 +1,13 @@
 ﻿Console.Clear();
-Console.Write("Введите кол-во элементов массива: ");
+Console.Write("Введите число: ");
 
-int[] size = Console.ReadLine()!.Split().Select(i => int.Parse(i)).ToArray();
-Console.WriteLine(size[0] + size[1]);
+// int[] size = Console.ReadLine()!.Split().Select(i => int.Parse(i)).ToArray();
+// Console.WriteLine(size[0] + size[1]);
+
+string number = Console.ReadLine()!;
+int result = 0;
+for(int i = 0; i < number.Length; i++)
+{
+  result += int.Parse(number[i].ToString());
+}
+Console.WriteLine(result);
