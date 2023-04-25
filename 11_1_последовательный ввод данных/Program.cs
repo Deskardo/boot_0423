5156649a36850b8cd4dc3a1ff8922780853dc78a
Deskardo <kalugin.aleksei.v@gmail.com>
2023-04-25 14:49:41 +0300

    task 11_1_input

---
+++ 11_1_последовательный ввод данных/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+
+int[] size = Console.ReadLine()!.Split().Select(i => int.Parse(i)).ToArray();
+Console.WriteLine(size[0] + size[1]);
